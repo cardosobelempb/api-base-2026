aaf34c6ca3062c71809da014c0617a843016884b
Claudio Cardoso <claudio.c.lima@hotmail.com>
2026-01-11 00:17:18 -0300

    27. Criando a abstração para o repositório InMemoryRepository - parte 4

--- src/common/domain/repositories/search.ts
+++ src/common/domain/repositories/search.ts
@@ -1,18 +1,18 @@
 export type SearchInput = {
-  page: number
+  page?: number
   perPage?: number // substitui size
   filter?: string
   sortBy?: string
   sortDirection?: 'asc' | 'desc'
 }
 
-export type SearchOutput<T> = {
-  items: T[]
+export type SearchOutput<Entity> = {
+  items: Entity[]
   total: number
-  perPage: number
-  currentPage: number
   totalPages: number
-  filter?: string
-  sortBy?: string
+  currentPage: number
+  perPage: number
+  sortBy?: string // opcional agora
   sortDirection?: 'asc' | 'desc'
+  filter: string
 }
